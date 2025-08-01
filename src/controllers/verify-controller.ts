8ca8bf07461ee209249da31b665105c9a6fd75d8
Ali Arman <ali@gmail.com>
2025-08-02 03:19:18 +0500

    Update cookie cewttring

--- src/controllers/verify-controller.ts
+++ src/controllers/verify-controller.ts
@@ -112,7 +112,12 @@ export const verifyCode = async (req: Request, res: Response) => {
       isVerified: true,
     };
     const token = jwt.sign(cookiePayload, process.env.JWT_SECRET!);
-    res.cookie("ecom_asis_cms", token);
+    res.cookie("ecom_asis_cms", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 1,
+    });
     res.status(201).json({ success: true, message: "Cookie created" });
   } catch (error) {
     console.log(error);
