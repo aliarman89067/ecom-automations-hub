5038213679c35c7cdc85ad381245c422b1aa7c41
Ali Arman <ali@gmail.com>
2025-08-02 02:18:23 +0500

    export app

--- dist/index.js
+++ dist/index.js
@@ -3,19 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-// index.ts
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const contract_route_1 = __importDefault(require("./route/contract-route"));
 const verify_route_1 = __importDefault(require("./route/verify-route"));
 const lead_controller_1 = require("./controllers/lead-controller");
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
 require("dotenv/config");
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
+// console.log("Origin", process.env.CLIENT_ENDPOINT);
 app.use((0, cors_1.default)({
     origin: process.env.CLIENT_ENDPOINT,
     credentials: true,
@@ -26,8 +25,9 @@ app.get("/test", (req, res) => {
     res.send("Hello World");
 });
 app.post("/generate-lead", lead_controller_1.generateLead);
-// Connect to MongoDB
-mongoose_1.default.connect(process.env.MONGO_URI);
-// 👇 Export handler for Vercel
-const serverless_http_1 = __importDefault(require("serverless-http"));
-exports.handler = (0, serverless_http_1.default)(app);
+mongoose_1.default.connect(process.env.MONGO_URI).then(() => {
+    app.listen(8080, () => {
+        console.log("Server is running on port 8080");
+    });
+});
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-// index.ts
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import ContractRouter from "./route/contract-route";
 import verifyRouter from "./route/verify-route";
 import { generateLead } from "./controllers/lead-controller";
-import cookieParser from "cookie-parser";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// console.log("Origin", process.env.CLIENT_ENDPOINT);
 
 app.use(
   cors({
@@ -19,17 +20,19 @@ app.use(
     credentials: true,
   })
 );
-
 app.use("/api/v1/contract", ContractRouter);
 app.use("/api/v1/verify", verifyRouter);
+
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
+
 app.post("/generate-lead", generateLead);
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI!);
+mongoose.connect(process.env.MONGO_URI!).then(() => {
+  app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+  });
+});
 
-// 👇 Export handler for Vercel
-import serverless from "serverless-http";
-export const handler = serverless(app);
+export default app;
